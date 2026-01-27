edbebf12849e49381165b7f2b07610318dcf4fc5
Ifeanyi Njoku <injoku@my.centennialcollege.ca>
2026-01-26 19:38:28 -0500

    Updated Project.js models logic

--- app/models/projects.js
+++ app/models/projects.js
@@ -3,9 +3,42 @@ const Schema = mongoose.Schema;
 
 const projectSchema = new Schema(
     {
-        title: String,
-        completion: Date,
-        description: String
+        title: {
+            title: String,
+            required: 'Title is required',
+            trim: true
+        },
+        // To make URL friendly, human readable, and cleaner
+        slug: {
+            type: String,
+            trim: true
+        },
+        completion: {
+            type: Date,
+            default: Date.now
+        },
+        description: {
+            type: String,
+            required: 'Description is required',
+            trim: true
+        },
+        tags: [{
+            type: String, //e.g., ["React Native", "Expo", "Python", etc.]
+            trim: true
+        }],
+        githubUrl: {
+            type: String,
+            trim: true
+        },
+        liveUrl: {
+            type: String,
+            trim: true
+        },
+        imageUrl: {
+            type: String,
+            default: "https://placehold.co/600x400?text=Project+Thumbnail"
+        }
+
     }
 );
 
