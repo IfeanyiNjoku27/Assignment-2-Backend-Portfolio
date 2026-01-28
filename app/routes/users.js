6a06583a1b3ed7a471abe0f0dc7c35963e6c72b4
Ifeanyi Njoku <injoku@my.centennialcollege.ca>
2026-01-27 19:24:02 -0500

    No Changes made

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,11 +5,10 @@ const auth = require('../controllers/auth.middleware');
 // Public Routes
 router.post('/signin', userController.signin);
 router.post('/signup', userController.create);
-
-
-// Protected Routes
 router.get('/', userController.getAll);
 router.get('/:id', userController.getById);
+
+// Protected Routes
 router.post('/', auth, userController.create);
 router.put('/:id', auth, userController.update);
 router.delete('/:id', auth, userController.delete);
